79261f5be81cbac468339b9ac06bca0045d2067f
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-12-07 15:17:18 -0800

    Removed canvas, helpers, and other unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
 import './App.css'
-import { useEffect, useRef } from 'react'
-import Canvas from './components/canvas/Canvas'
 import Pieces from './components/pieces/Pieces'
 
 function App() {
-  const draw = (ctx) => {
-    console.log(pieceT.piece)
-    const tileSize = 30
-    for (let y = 0; y < pieceT.piece.length; y++) {
-      for (let x = 0; x < pieceT.piece[y].length; x++) {
-        ctx.fillStyle = 'red'
-        if (pieceT.piece[y][x] === 1) ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize)
-      }
-    }
-  }
-  
   return (
     <>
       <div className="heading-container">
         <h1>Puzzle a Day</h1>
       </div>
-
-      {/* <div ref={refContainer}></div> */}
-
-      {/* <Canvas
-        width='800'
-        height='800' /> */}
-
       <Pieces />
     </>
   )
